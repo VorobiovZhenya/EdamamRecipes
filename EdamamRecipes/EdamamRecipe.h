a52a456cd5d7fd8201e194df19f76b63a6387f71
Zhenya <zhenya@imac-zhenya.local>
2017-03-03 14:07:30 +0200

    added new class

---
+++ EdamamRecipes/EdamamRecipe.h
@@ -0,0 +1,16 @@
+//
+//  EdamamRecipe.h
+//  EdamamRecipes
+//
+//  Created by Zhenya on 3/3/17.
+//  Copyright © 2017 Zhenka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EdamamRecipe : NSObject
+{
+
+}
+
+@end
---
+++ EdamamRecipes/EdamamRecipe.m
@@ -0,0 +1,13 @@
+//
+//  EdamamRecipe.m
+//  EdamamRecipes
+//
+//  Created by Zhenya on 3/3/17.
+//  Copyright © 2017 Zhenka. All rights reserved.
+//
+
+#import "EdamamRecipe.h"
+
+@implementation EdamamRecipe
+
+@end
